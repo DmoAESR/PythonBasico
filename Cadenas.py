a3b3a233d37c132e93e6343ca27d3176234dc5cb
Ricardo Arreola Sanchez <ricardo.arreola@docsolutions.com>
2024-06-12 19:31:20 -0600

    Primeros archivos python

---
+++ Cadenas.py
@@ -0,0 +1,26 @@
+cadena = "Hola mundo!"
+simple = 'Adios tierra?'
+
+print(cadena[1])
+print(cadena[:7])
+print(cadena[5:])
+print(cadena[-4:])
+print(cadena[:-4])
+
+print(cadena + " " + simple)
+
+ruta = "C:\""
+rutaD = "C:\\Documentos"
+
+print(ruta)
+print(rutaD)
+
+# Interpretar de forma simple
+cadenadirecta = r"C:\Documentos"
+print(cadenadirecta)
+
+
+print(len(cadena))
+
+print(cadena.lower())
+print(cadena.upper())
---
+++ TiposDatos.py
@@ -0,0 +1,22 @@
+entero = 1
+print(type(entero))
+
+booleano = True
+print(type(booleano))
+
+flotantes = 3.1416
+print(type(flotantes))
+
+complejoA = 2 + 4j
+complejoB = 4 + 7j
+
+resultado = complejoA + complejoB
+print(resultado)
+
+sumoenteroflotante = entero + flotantes
+print(sumoenteroflotante)
+
+cadena = "Hola Mundo"
+print(cadena+str(entero))
+
+print(type(entero))
